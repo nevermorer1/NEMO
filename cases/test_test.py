27a070da9020993ae96b254273b4b2cb67aa36f8
mabao@juzix.io <mabao@juzix.io>
2018-06-19 16:00:03 +0800

    add

---
+++ cases/test_test.py
@@ -0,0 +1,24 @@
+from dataHandle import DataHandle
+from loadConfig import LoadConfig
+import unittest
+
+
+class MMM(unittest.TestCase):
+    def setUp(self):
+        self.lc = LoadConfig()
+        self.domain = self.lc.get_domain()
+        self.dh = DataHandle()
+        pass
+
+    def test_common_method(self):
+        """test framework"""
+        url = self.lc.get_domain() + self.dh.get_path(3)
+        print('request url is %s' % url)
+        para_source = self.dh.get_para(3)
+        data_source = self.dh.get_data(3)
+        for i in range(len(data_source)):
+            req_para = DataHandle.combine_data(para_source, data_source[i])
+            print(req_para)
+            actual = 33
+            DataHandle.set_data(data_source[i], actual)
+        self.dh.write_data(data_source)
